7f793b45b6b4f88737006c8610e3f4790268ad03
Sal Rahman <salehen.rahman@gmail.com>
2019-10-05 19:53:26 -0700

    Removed some additional stuff.

--- background.js
+++ background.js
@@ -4,9 +4,20 @@ const hide = (el) => {
   }
 }
 
+const hideAll = (els) => {
+  for (const el of els) {
+    hide(el);
+  }
+}
+
 window.addEventListener('DOMContentLoaded', function () {
   setInterval(() => {
-    hide(document.querySelectorAll('ytd-watch-next-secondary-results-renderer'))
+    hideAll(document.querySelectorAll('ytd-watch-next-secondary-results-renderer'))
+    hideAll(document.querySelectorAll('.ytp-ce-covering-overlay'))
+    hideAll(document.querySelectorAll('.ytp-ce-element.ytp-ce-video.ytp-ce-element-show.ytp-ce-top-right-quad'))
+    hideAll(document.querySelectorAll('.ytp-ce-element.ytp-ce-video.ytp-ce-element-show.ytp-ce-top-left-quad'))
+    hideAll(document.querySelectorAll('.ytp-ce-element.ytp-ce-channel.ytp-ce-channel-this.ytp-ce-top-right-quad.ytp-ce-element-show'))
+    hideAll(document.querySelectorAll('.ytp-videowall-still.ytp-suggestion-set'))
     hide(document.querySelector('#feed, ytd-browse[page-subtype=home]'))
     hide(document.querySelector('#related'));
   }, 32);
